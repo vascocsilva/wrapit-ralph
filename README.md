cb329bcc968db8239c187da7957beb9593656b79
Vasco Silva <vasco.silva@tekzenit.com>
2017-06-05 07:55:09 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# wrapit-ralph package
+
+Atom Package to wrap text on lines no longer than 80 characters.
+(Still a work in progress)
---
+++ spec/wrapit-ralph-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WrapitRalphView from '../lib/wrapit-ralph-view';
+
+describe('WrapitRalphView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
